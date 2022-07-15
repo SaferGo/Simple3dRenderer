f8814c2da4dc9f93a8084b0fa48f28526afe84bd
safergo <sfgomez1998@gmail.com>
2022-07-15 03:51:11 +0200

    Removed folders from repository

--- build/libs/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/code/graphics/Simple-3D-Renderer/libs/glm
-# Build directory: /home/santiago/code/graphics/Simple-3D-Renderer/build/libs/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/libs/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/code/graphics/Simple-3D-Renderer/libs/glm/glm
-# Build directory: /home/santiago/code/graphics/Simple-3D-Renderer/build/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
