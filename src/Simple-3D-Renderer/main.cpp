247f1f7a56929670e6c1768943877e1e4ac88130
safergo <sfgomez1998@gmail.com>
2022-07-15 18:14:04 +0200

    pixel testing

--- src/Simple-3D-Renderer/main.cpp
+++ src/Simple-3D-Renderer/main.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
+   TGAImage image(100, 100, TGAImage::RGB);
+   image.set(52, 41, TGAColor(255, 0, 0, 255));
+   image.flip_vertically();
+   image.write_tga_file("output.tga");
+
    return 0;
 }
